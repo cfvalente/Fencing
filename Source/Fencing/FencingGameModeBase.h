5a4aca25ae9895affba01b3ee3a38b682e2b21fc
cfvalente <caio99valente@hotmail.com>
2016-12-01 20:13:10 -0200

    Initial commit

---
+++ Source/Fencing/FencingGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "FencingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FENCING_API AFencingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
